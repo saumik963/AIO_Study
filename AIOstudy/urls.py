90c13f35ef8f750bfe4342ce2234c272c8391972
Saumik Das <119744035+saumik963@users.noreply.github.com>
2023-10-03 00:03:24 +0600

    Update urls.py

--- AIOstudy/urls.py
+++ AIOstudy/urls.py
@@ -19,15 +19,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-from django.urls import re_path as url
 from django.conf.urls import handler404, handler500
 from Study.views import error_404, error_500
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
-    url(r'^media/(?P<path>.*)$', serve,
-        {'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,
-        {'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
     path('', include('Study.urls')),
