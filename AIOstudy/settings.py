3359f4874db102af2b1ed070fd83c9039831d70f
saumik963 <saumikdas33@gmail.com>
2024-02-12 13:16:58 +0600

    Update Features

--- AIOstudy/settings.py
+++ AIOstudy/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'account.UserProfile'
 
